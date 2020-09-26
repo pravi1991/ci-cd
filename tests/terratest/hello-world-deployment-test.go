25729101bfd6a438cab7653d3ae4307d1754da8a
Praveen TT <pravi.aroli@gmail.com>
2020-09-27 01:48:40 +0530

    all

--- tests/terratest/hello-world-deployment-test.go
+++ tests/terratest/hello-world-deployment-test.go
@@ -1,21 +1,31 @@
+// +build kubeall kubernetes
+// NOTE: See the notes in the other Kubernetes example tests for why this build tag is included.
+
 package test
 
 import (
+	"fmt"
 	"testing"
+	"time"
 
-	"github.com/gruntwork-io/terratest/modules/terraform"
-	"github.com/stretchr/testify/assert"
+	http_helper "github.com/gruntwork-io/terratest/modules/http-helper"
+	"github.com/gruntwork-io/terratest/modules/k8s"
 )
 
-func TestTerraformHelloWorldExample(t *testing.T) {
-	terraformOptions := &terraform.Options{
-		TerraformDir: "../examples/terraform-hello-world-example",
-	}
+func TestKubernetesHelloWorldExample(t *testing.T) {
+	t.Parallel()
 
-	defer terraform.Destroy(t, terraformOptions)
+	kubeResourcePath := "../examples/kubernetes-hello-world-example/hello-world-deployment.yml"
 
-	terraform.InitAndApply(t, terraformOptions)
+	options := k8s.NewKubectlOptions("", "", "default")
 
-	output := terraform.Output(t, terraformOptions, "hello_world")
-	assert.Equal(t, "Hello, World!", output)
+	defer k8s.KubectlDelete(t, options, kubeResourcePath)
+
+	k8s.KubectlApply(t, options, kubeResourcePath)
+
+	k8s.WaitUntilServiceAvailable(t, options, "hello-world-service", 10, 1*time.Second)
+	service := k8s.GetService(t, options, "hello-world-service")
+	url := fmt.Sprintf("http://%s", k8s.GetServiceEndpoint(t, options, service, 5000))
+
+	http_helper.HttpGetWithRetry(t, url, nil, 200, "Hello world!", 30, 3*time.Second)
 }
