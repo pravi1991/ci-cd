36bdc53342fa771ad19474718891de45e473f8a5
Praveen TT <pravi.aroli@gmail.com>
2020-09-28 12:35:06 +0530

    all

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 alias kubectl='kubectl --kubeconfig ~/.kube/kubeconfig'
 for i in {0..2} ; do 
-    number=$i envsubst < minikube-pv.yaml| kubectl apply -f -
+    number=$i envsubst < k8s-manifests/minikube-pv.yaml| kubectl apply -f -
  done
 kubectl apply -f setup.yaml
 kubectl apply -f k8s-manifests/elastic.secret.password.yaml
---
+++ tests/infraTesting/elastic_infratest.py
@@ -0,0 +1,21 @@
+import pytest
+@pytest.fixture
+def kubeconfig():
+    return '~/.kube/kubeconfig'
+@pytest.mark.namespace(create=False, name='monitoring')
+@pytest.mark.applymanifest('k8s-manifests/elasticsearch.config.yaml')
+@pytest.mark.applymanifest('k8s-manifests/readiness.elastic.cm.yaml')
+@pytest.mark.applymanifest('k8s-manifests/elasticsearch.yaml')
+def test_es(kube):
+    kube.wait_for_registered()
+    sts = kube.get_statefulsets('monitoring')
+    service = kube.get_services('monitoring')
+    elastic_service = service.get('elasticsearch-logging')
+    elasticsearch_sts = sts.get('elasticsearch-master')
+    assert elastic_service is not None
+    assert elasticsearch_sts is not None
+    pods = elasticsearch_sts.get_pods()
+    assert len(pods) == 3, 'pods should deploy with three replicas'
+    for pod in pods:
+        resp = pod.http_proxy_get('/_cluster/health?local=true')
+        assert 'green' in resp.data
